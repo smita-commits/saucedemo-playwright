5b02764405eb6927786044da850a64d9fc9489c9
smita-commits <107355098+smita-commits@users.noreply.github.com>
2025-06-29 20:45:39 +0530

    Added relevant comments and an assertion for  inventory.html navigation cart-flow-test.spec.ts

--- tests/cart-flow-test.spec.ts
+++ tests/cart-flow-test.spec.ts
@@ -9,16 +9,28 @@ test('Login, add product to cart by name, capture cart screenshot, and logout', 
   const cartPage = new CartPage(page);
 
   const productName = 'Sauce Labs Bolt T-Shirt';
-
+  //Go to the login page and login with valid credentials
   await loginPage.goto();
   await loginPage.login();
 
+  // Assert navigation to inventory.html
+  await expect(page).toHaveURL(/.*inventory\.html/);
+
+  //Add product by name
   await inventoryPage.addProductToCart(productName);
+
+  //Go to cart
   await inventoryPage.goToCart();
 
+  //Verify Product in cart
   await cartPage.verifyProductInCart(productName);
+
+  //Take screenshot of the cart page
   await cartPage.takeScreenshot('cart-screenshot.png');
+
+  //Logout from menu
   await cartPage.logout();
 
+  //Confirm logout
   await expect(page).toHaveURL('/');
 });
